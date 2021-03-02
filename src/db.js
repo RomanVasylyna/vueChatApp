4422d10a51253bea2bc431d05b008f230eab1fad
Roman <romanvasylyna888@gmail.com>
2021-03-02 17:33:18 +0200

    Empty messages

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+// Getting db api key
+const config = {
+    // API KEYS
+    apiKey: "AIzaSyDcXLI4Q1IXXmw41NApHQLKPv7jyDh2UJU",
+    authDomain: "vue-chat-app-a5f62.firebaseapp.com",
+    projectId: "vue-chat-app-a5f62",
+    storageBucket: "vue-chat-app-a5f62.appspot.com",
+    messagingSenderId: "657715972583",
+    appId: "1:657715972583:web:3c012b81cad9e2f763d542"
+};
+
+// Initialize db
+const db = firebase.initializeApp(config);
+
+// Export db to use it withing the APP
+export default db;
